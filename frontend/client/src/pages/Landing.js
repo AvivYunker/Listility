3977ac50631aab5277cf588032da3ee4646020ee
AvivYunker <avivyunker@gmail.com>
2022-02-05 17:17:47 +0200

    finish Landing.js page

--- frontend/client/src/pages/Landing.js
+++ frontend/client/src/pages/Landing.js
@@ -1,12 +1,12 @@
 import logo from '../assets/images/logo.svg';
 import main from '../assets/images/main.svg';
-
+import styled from 'styled-components';
 
 import React from 'react';
 
 const Landing = () => {
   return(
-      <main>
+      <Wrapper>
           <nav>
               <img src={logo} alt="listility" className="logo"/>
           </nav>
@@ -24,8 +24,46 @@ const Landing = () => {
               </div>
               <img src={main} alt="job hunt" className="img main-img"/>
           </div>
-      </main>
+      </Wrapper>
   );
 };
+
+const Wrapper = styled.main`
+    nav {
+        width: var(--fluid-width);
+        max-width: var(--max-width);
+        margin: 0 auto;
+        height: var(--nav-height);
+        display: flex;
+        align-items: center;
+    }
+    .page {
+        min-height: calc(100vh - var(--nav-height));
+        display: grid;
+        align-items: center;
+        margin-top: -3rem;
+    }
+    h1 {
+        font-weight: 700;
+        span {
+            color: var(--primary-500);
+        }
+    }
+    p {
+        color: var(--grey-600);
+    }
+    .main-img {
+        display: none;
+    }
+    @media (min-width: 992px) {
+        .page {
+            grid-template-columns: 1fr 1fr;
+            column-gap: 3rem;
+        }
+        .main-img {
+            display: block;
+        }
+    }
+`
 
 export default Landing;
