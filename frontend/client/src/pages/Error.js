3b28fce61b9f3baa878b0ba840c99b41f8c8aa04
AvivYunker <avivyunker@gmail.com>
2022-02-07 13:32:21 +0200

    develop error page

--- frontend/client/src/pages/Error.js
+++ frontend/client/src/pages/Error.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/ErrorPage';
+import img from '../assets/images/not-found.svg';
+
 
 const Error = () => {
   return(
-    <h1>
-        Error
-    </h1>
+      <Wrapper className="full-page">
+          <div>
+              <img src={img} alt="not found"/>
+              <h3>Ohh! page not found</h3>
+              <p>We can't seem to find the page you're looking for</p>
+              <Link to="/">Back Home</Link>
+          </div>
+      </Wrapper>
   );
 };
 
