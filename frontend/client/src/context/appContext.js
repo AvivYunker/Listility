140e24eb0ab29d758e5b796085578d13e6f3b919
AvivYunker <avivyunker@gmail.com>
2022-02-09 11:43:57 +0200

    edit alert to toggle (33)

--- frontend/client/src/context/appContext.js
+++ frontend/client/src/context/appContext.js
@@ -1,5 +1,6 @@
 import React, {useReducer, useContext } from 'react'
 import reducer from './reducer';
+import { DISPLAY_ALERT } from './actions';
 
 const initialState = {
     isLoading: false,
@@ -12,7 +13,16 @@ const AppContext = React.createContext()
 
 const AppProvider = ({children}) => {
     const [state, dispatch] = useReducer(reducer, initialState);
-    return (<AppContext.Provider value={{...state}}>
+
+    const AppProvider = ({children}) => {
+        const [state, dispatch] = useReducer(reducer, initialState);
+    }
+
+    const displayAlert = () => {
+        dispatch({type: DISPLAY_ALERT});
+    }
+
+    return (<AppContext.Provider value={{...state, displayAlert}}>
         {children}
     </AppContext.Provider>);
 }
