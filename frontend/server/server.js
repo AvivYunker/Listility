54582280c2b00b798ab42ca1d352fe0d6dee159e
AvivYunker <avivyunker@gmail.com>
2022-02-09 14:30:44 +0200

    add error middleware (40)

--- frontend/server/server.js
+++ frontend/server/server.js
@@ -2,14 +2,17 @@ import express from 'express'
 const app = express()
 
 // middleware
-notFoundMiddleware
+
 import notFoundMiddleware from './middleware/not-found.js'
+import errorHandlerMiddleware from './middleware/error-handler.js'
 
 app.get('/', (req,res) => {
+    throw new Error('error');
     res.send("Welcome!");
 });
 
 app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000
 
