f9e128d41ccb902648c73ee46c27734b62ecb042
AvivYunker <avivyunker@gmail.com>
2022-02-09 14:57:27 +0200

    connect to MongoDB, add url to .env (42)

--- frontend/server/server.js
+++ frontend/server/server.js
@@ -4,13 +4,14 @@ import dotenv from 'dotenv'
 const app = express()
 dotenv.config()
 
+import connectDB from './db/connect.js'
 // middleware
 
 import notFoundMiddleware from './middleware/not-found.js'
 import errorHandlerMiddleware from './middleware/error-handler.js'
 
 app.get('/', (req,res) => {
-    throw new Error('error');
+    // throw new Error('error');
     res.send("Welcome!");
 });
 
@@ -19,6 +20,15 @@ app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000
 
-app.listen(port, ()=>{
-    console.log(`Server is listening on port ${port}...`);
-});
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URL)
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}...`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
