d3eceb7ae090af16e094c6658a31dd0a470ab166
AvivYunker <avivyunker@gmail.com>
2022-02-09 14:23:37 +0200

    add middleware-not-found middleware (39)

--- frontend/server/server.js
+++ frontend/server/server.js
@@ -1,9 +1,15 @@
 import express from 'express'
 const app = express()
 
+// middleware
+notFoundMiddleware
+import notFoundMiddleware from './middleware/not-found.js'
+
 app.get('/', (req,res) => {
     res.send("Welcome!");
 });
+
+app.use(notFoundMiddleware)
 
 const port = process.env.PORT || 5000
 
