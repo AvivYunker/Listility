6771ef2eb58b510447df18596b8c9bd611c07ec7
AvivYunker <avivyunker@gmail.com>
2022-02-10 21:02:08 +0200

    add register error-handling (48)

--- frontend/server/controllers/authController.js
+++ frontend/server/controllers/authController.js
@@ -1,12 +1,19 @@
-const register = (req, res) => {
-    res.send('register user');
+import User from '../models/User.js'
+
+const register =  async (req, res) => {
+    try {
+        const user = await User.create(req.body);
+        res.status(201).json({user});
+    } catch (error) {
+        res.status(500).json({ msg: "there was an error" });
+    }
 }
 
-const login = (req, res) => {
+const login = async (req, res) => {
     res.send('login user');
 }
 
-const updateUser = (req, res) => {
+const updateUser = async (req, res) => {
     res.send('register user');
 }
 
