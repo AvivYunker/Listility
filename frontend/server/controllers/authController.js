1e061e25bc2724f18e83ec1695e0b382ec713b08
AvivYunker <avivyunker@gmail.com>
2022-02-11 22:42:40 +0200

    add login controller

--- frontend/server/controllers/authController.js
+++ frontend/server/controllers/authController.js
@@ -9,7 +9,7 @@ const register =  async (req, res) => {
         throw new BadRequestError('please provide all values');
     }
 
-    const userAlreadyExists = await User.findOne({email})
+    const userAlreadyExists = await User.findOne({email}).select('+password')
     if (userAlreadyExists) {
         throw new BadRequestError('Email already in use')
     }
@@ -29,7 +29,23 @@ const register =  async (req, res) => {
 }
 
 const login = async (req, res) => {
-    res.send('login user');
+    const {email, password} = req.body;
+    if (!email || !password) {
+        throw new BadRequestError('Please provide all values');
+    }
+    const user = await User.findOne({ email })
+    if (!user) {
+        throw new UnAuthenticatedError('Invalid Credentials')
+    }
+    console.log(user);
+
+    const isPasswordCorrect = await user.comparePassword(password);
+    if (!isPasswordCorrect) {
+        throw new UnAuthenticatedError("Invalid Credentials");
+    }
+    const token = user.createJWT()
+    user.password = undefined
+    res.status(StatusCodes.OK).json({user, token, location: user.location})
 }
 
 const updateUser = async (req, res) => {
