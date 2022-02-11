ad9d95f22e69fb3fc13f8add3facb8d5810ce590
AvivYunker <avivyunker@gmail.com>
2022-02-11 09:37:11 +0200

    check for empty values (55)

--- frontend/server/controllers/authController.js
+++ frontend/server/controllers/authController.js
@@ -2,8 +2,14 @@ import User from '../models/User.js'
 import { StatusCodes } from 'http-status-codes'
 
 const register =  async (req, res) => {
-    const user = await User.create(req.body);
-    res.status(StatusCodes.OK).json({user});
+    const {name, email, password} = req.body
+
+    if (!name || !email || !password) {
+        throw new Error("Please provide all values");
+    }
+
+    const user = await User.create({ name, email, password });
+    res.status(StatusCodes.OK).json({ user });
 }
 
 const login = async (req, res) => {
