ed3780b07e3e977871a6795dc7bb2d8f5a895c47
AvivYunker <avivyunker@gmail.com>
2022-02-11 10:47:57 +0200

    fix import errors on error-classes

--- frontend/server/errors/custom-api.js
+++ frontend/server/errors/custom-api.js
@@ -1,6 +1,3 @@
-import { StatusCodes } from 'http-status-codes'
-import CustomAPIError from './custom-api'
-
 class CustomAPIError extends Error {
     constructor(message) {
         super(message)
