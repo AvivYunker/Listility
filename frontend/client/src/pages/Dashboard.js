8533eb2436c828c5453b69acfc78505ce164cc4e
AvivYunker <avivyunker@gmail.com>
2022-02-11 13:38:59 +0200

    implement fetch error in dashboard

--- frontend/client/src/pages/Dashboard.js
+++ frontend/client/src/pages/Dashboard.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import { useEffect } from 'react'
 
 const Dashboard = () => {
-  return(
-    <h1>
-        Dashboard Page
-    </h1>
-  );
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:4000')
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    fetchData()
+  }, [])
+  return <h1>Dashboard Page</h1>
 };
 
 export default Dashboard;
