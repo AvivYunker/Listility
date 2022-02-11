f7bc279c5f189d05018011ce7bd044612b7393be
AvivYunker <avivyunker@gmail.com>
2022-02-11 19:21:53 +0200

    allow navigation to dashboard once register successful (78)

--- frontend/client/src/pages/Register.js
+++ frontend/client/src/pages/Register.js
@@ -3,6 +3,7 @@ import { useState, useEffect} from 'react';
 import { Logo, FormRow, Alert } from '../components'; 
 import Wrapper from '../assets/wrappers/RegisterPage';
 import { useAppContext } from '../context/appContext';
+import { useNavigate } from 'react-router-dom';
 
 const initialState = {
     name: '',
@@ -12,10 +13,10 @@ const initialState = {
 }
 
 const Register = () => {
+    const navigate = useNavigate()
     const [values, setValues] = useState(initialState);
-    // global state and useNavigate
 
-    const {isLoading, showAlert, displayAlert, registerUser } = useAppContext();
+    const { user, isLoading, showAlert, displayAlert, registerUser } = useAppContext();
 
     const toggleMember = () => {
         setValues({...values, isMember:!values.isMember})
@@ -39,6 +40,15 @@ const Register = () => {
         }
         console.log(values);
     }
+
+    useEffect(() => {
+        if (user) {
+            setTimeout(() => {
+                navigate('/')
+            }, 3000);
+        }
+    }, [user, navigate])
+
     return ( <Wrapper className="full-page">
         <form className="form" onSubmit={onSubmit}>
             <Logo/>
