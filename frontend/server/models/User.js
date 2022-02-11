d64188b0139388abaa78cedf033dacd9149231f8
AvivYunker <avivyunker@gmail.com>
2022-02-11 22:25:56 +0200

    compare hash password in user (82)

--- frontend/server/models/User.js
+++ frontend/server/models/User.js
@@ -50,4 +50,9 @@ UserSchema.methods.createJWT = function() {
     return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_LIFETIME});
 }
 
+UserSchema.methods.comparePassword = async function(candidatePassword) {
+    const isMatch = await bcrypt.compare(candidatePassword, this.password);
+    return isMatch
+}
+
 export default mongoose.model('User', UserSchema);
