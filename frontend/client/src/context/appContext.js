aba5cec732f44d2b4519a763e6ff0e458d184776
AvivYunker <avivyunker@gmail.com>
2022-02-12 09:28:07 +0200

    refactor login-user

--- frontend/client/src/context/appContext.js
+++ frontend/client/src/context/appContext.js
@@ -10,6 +10,9 @@ import {
     LOGIN_USER_BEGIN,
     LOGIN_USER_SUCCESS,
     LOGIN_USER_ERROR,
+    SETUP_USER_BEGIN,
+    SETUP_USER_SUCCESS,
+    SETUP_USER_ERROR,
 } from "./actions";
 
 const token = localStorage.getItem('token')
@@ -82,6 +85,25 @@ const AppProvider = ({children}) => {
         clearAlert()
     }
     
+    const setupUser = async ({currentUser, endPoint, alertText}) => {
+        dispatch({ type: SETUP_USER_BEGIN })
+        try {
+            const {data} = await axios.post(`/api/v1/auth/${endPoint}`, currentUser)
+            const {user, token, location} = data
+            dispatch({
+                type: SETUP_USER_SUCCESS,
+                payload: {user, token, location, alertText},
+            })
+            addUserToLocalStorage({user, token, location})
+        } catch (error) {
+            dispatch({
+                type: SETUP_USER_ERROR,
+                payload: {msg: error.response.data.msg}
+            })
+        }
+        clearAlert()
+    }
+
     const loginUser = async (currentUser) => {
         dispatch({ type: LOGIN_USER_BEGIN })
         try {
@@ -102,7 +124,7 @@ const AppProvider = ({children}) => {
     }
 
     return (
-    <AppContext.Provider value={{ ...state, displayAlert, registerUser, loginUser }}>
+    <AppContext.Provider value={{ ...state, displayAlert, registerUser, loginUser, setupUser }}>
         {children}
     </AppContext.Provider>
     );
--- frontend/client/src/context/reducer.js
+++ frontend/client/src/context/reducer.js
@@ -7,6 +7,9 @@ import {
     LOGIN_USER_BEGIN,
     LOGIN_USER_SUCCESS,
     LOGIN_USER_ERROR,
+    SETUP_USER_BEGIN,
+    SETUP_USER_SUCCESS,
+    SETUP_USER_ERROR,
     } from "./actions";
 
 const reducer = (state,action) => {
@@ -33,10 +36,10 @@ const reducer = (state,action) => {
         return {
             ...state,
             isLoading: false,
-            // token: action.payload.token,
-            // user: action.payload.user,
-            // userLocation: action.payload.location,
-            // jobLocation: action.payload.location,
+            token: action.payload.token,
+            user: action.payload.user,
+            userLocation: action.payload.location,
+            jobLocation: action.payload.location,
             showAlert: true,
             alertType: 'Register Successful! Redirecting...',
             alertText: action.payload.msg,
@@ -58,16 +61,41 @@ const reducer = (state,action) => {
         return {
             ...state,
             isLoading: false,
-            // token: action.payload.token,
-            // user: action.payload.user,
-            // userLocation: action.payload.location,
-            // jobLocation: action.payload.location,
+            token: action.payload.token,
+            user: action.payload.user,
+            userLocation: action.payload.location,
+            jobLocation: action.payload.location,
             showAlert: true,
             alertType: 'Login Successful! Redirecting...',
             alertText: action.payload.msg,
         }
     }
     if (action.type === LOGIN_USER_ERROR) {
+        return {
+            ...state,
+            isLoading: false,
+            showAlert: true,
+            alertType: 'danger',
+            alertText: action.payload.msg,
+        }
+    }
+    if (action.type === SETUP_USER_BEGIN) {
+        return {...state, isLoading: true}
+    }
+    if (action.type === SETUP_USER_SUCCESS) {
+        return {
+            ...state,
+            isLoading: false,
+            token: action.payload.token,
+            user: action.payload.user,
+            userLocation: action.payload.location,
+            jobLocation: action.payload.location,
+            showAlert: true,
+            alertType: 'success',
+            alertText: action.payload.alertText,
+        }
+    }
+    if (action.type === SETUP_USER_ERROR) {
         return {
             ...state,
             isLoading: false,
