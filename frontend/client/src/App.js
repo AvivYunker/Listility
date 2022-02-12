24369aa3bc1e35418b6008a7fc3dd78660114752
AvivYunker <avivyunker@gmail.com>
2022-02-12 09:54:52 +0200

    import nested dashboard pages to app.js

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,13 +1,18 @@
 import styled from 'styled-components';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import {Dashboard, Landing, Error, Register} from './pages';
-export {AddJob, AllJobs, Profile, SharedLayout, Stats} from './pages/Dashboard'
+import {AddJob, AllJobs, Profile, SharedLayout, Stats} from './pages/Dashboard';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>Dashboard</div>}/>
+        <Route path="/"/>
+          <Route path="stats" element={<Stats/>}/>
+          <Route path="all-jobs" element={<AllJobs/>}/>
+          <Route path="add-job" element={<AddJob/>}/>
+          <Route path="profile" element={<Profile/>}/>
+        <Route/>
         <Route path="/register" element={<Register/>}/>
         <Route path="/landing" element={<Landing/>}/>
         <Route path="*" element={<Error/>}/>
--- frontend/client/src/pages/Dashboard/index.js
+++ frontend/client/src/pages/Dashboard/index.js
@@ -4,10 +4,4 @@ import Profile from "./Profile";
 import SharedLayout from "./SharedLayout";
 import Stats from "./Stats";
 
-export {
-    AddJob,
-    AllJobs,
-    Profile,
-    SharedLayout,
-    Stats,
-}
+export { AddJob, AllJobs, Profile, SharedLayout, Stats }
