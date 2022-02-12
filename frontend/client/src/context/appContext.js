12a63f2c0328d416a575aaa2df532846dd7476e6
AvivYunker <avivyunker@gmail.com>
2022-02-12 09:12:42 +0200

    complete user-login functionality (85)

--- frontend/client/src/context/appContext.js
+++ frontend/client/src/context/appContext.js
@@ -1,7 +1,16 @@
 import React, {useReducer, useContext } from 'react'
 import reducer from './reducer';
 import axios from 'axios';
-import { DISPLAY_ALERT, CLEAR_ALERT, REGISTER_USER_BEGIN, REGISTER_USER_SUCCESS, REGISTER_USER_ERROR } from "./actions";
+import {
+    DISPLAY_ALERT,
+    CLEAR_ALERT,
+    REGISTER_USER_BEGIN,
+    REGISTER_USER_SUCCESS,
+    REGISTER_USER_ERROR,
+    LOGIN_USER_BEGIN,
+    LOGIN_USER_SUCCESS,
+    LOGIN_USER_ERROR,
+} from "./actions";
 
 const token = localStorage.getItem('token')
 const user = localStorage.getItem('user')
@@ -72,9 +81,24 @@ const AppProvider = ({children}) => {
         }
         clearAlert()
     }
-
+    
     const loginUser = async (currentUser) => {
-        console.log(currentUser)
+        dispatch({ type: LOGIN_USER_BEGIN })
+        try {
+            const {data} = await axios.post('/api/v1/auth/login', currentUser)
+            const {user, token, location} = data
+            dispatch({
+                type: LOGIN_USER_SUCCESS,
+                payload: {user, token, location},
+            })
+            addUserToLocalStorage({user, token, location})
+        } catch (error) {
+            dispatch({
+                type: LOGIN_USER_ERROR,
+                payload: {msg: error.response.data.msg}
+            })
+        }
+        clearAlert()
     }
 
     return (
--- frontend/client/src/context/reducer.js
+++ frontend/client/src/context/reducer.js
@@ -38,6 +38,40 @@ const reducer = (state,action) => {
             // userLocation: action.payload.location,
             // jobLocation: action.payload.location,
             showAlert: true,
+            alertType: 'Register Successful! Redirecting...',
+            alertText: action.payload.msg,
+        }
+    }
+    if (action.type === REGISTER_USER_ERROR) {
+        return {
+            ...state,
+            isLoading: false,
+            showAlert: true,
+            alertType: 'danger',
+            alertText: action.payload.msg,
+        }
+    }
+    if (action.type === LOGIN_USER_BEGIN) {
+        return {...state, isLoading: true}
+    }
+    if (action.type === LOGIN_USER_SUCCESS) {
+        return {
+            ...state,
+            isLoading: false,
+            // token: action.payload.token,
+            // user: action.payload.user,
+            // userLocation: action.payload.location,
+            // jobLocation: action.payload.location,
+            showAlert: true,
+            alertType: 'Login Successful! Redirecting...',
+            alertText: action.payload.msg,
+        }
+    }
+    if (action.type === LOGIN_USER_ERROR) {
+        return {
+            ...state,
+            isLoading: false,
+            showAlert: true,
             alertType: 'danger',
             alertText: action.payload.msg,
         }
