dd61f6421dfe53801c82557f393626060efc5b43
AvivYunker <avivyunker@gmail.com>
2022-02-12 12:49:24 +0200

    set the structure of the navbar (96)

--- frontend/client/src/components/Navbar.js
+++ frontend/client/src/components/Navbar.js
@@ -1,13 +1,42 @@
 import Wrapper from '../assets/wrappers/Navbar';
-import {FaHHome} from 'react-icons/fa';
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from 'react-icons/fa';
+import { userAppContext } from '../context/appContext';
+import Logo from './Logo';
 
 import React from 'react'
 
 const Navbar = () => {
   return (
     <Wrapper>
-        <h4></h4>
-        <FaHome/>
+      <div className="nav-center">
+        <button className="toggle-btn" onClick={() => console.log("toggle sidebar")}>
+          <FaAlignLeft/>
+        </button>
+        <div>
+          <Logo/>
+          <h3 className="logo-text">dashboard</h3>
+        </div>
+        <div className="btn-container">
+          <button
+          type="button"
+          className="btn"
+          onClick={() => console.log('show/hide dropdown')}
+          >
+            <FaUserCircle/>
+            john
+            <FaCaretDown/>
+          </button>
+          <div className="dropdown show-dropdown">
+            <button
+            type="button"
+            className="dropdown-btn"
+            onClick={() => console.log("logout user")}
+            >
+              logout
+            </button>
+          </div>
+        </div>
+      </div>
     </Wrapper>
   )
 }
