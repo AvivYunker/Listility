9231c8b37f89f5d016810340c60e2b3ae6c5aee7
AvivYunker <avivyunker@gmail.com>
2022-02-12 09:45:43 +0200

    add sub-pages for dashboard page (88)

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,12 +1,13 @@
 import styled from 'styled-components';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import {Dashboard, Landing, Error, Register} from './pages';
+export {AddJob, AllJobs, Profile, SharedLayout, Stats} from './pages/Dashboard'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard/>}/>
+        <Route path="/" element={<div>Dashboard</div>}/>
         <Route path="/register" element={<Register/>}/>
         <Route path="/landing" element={<Landing/>}/>
         <Route path="*" element={<Error/>}/>
