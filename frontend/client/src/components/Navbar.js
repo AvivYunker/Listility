2184141a248c3c92e13d6463ca20388bbf1837c4
AvivYunker <avivyunker@gmail.com>
2022-02-12 12:27:06 +0200

    add structure of lists page (93)

---
+++ frontend/client/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import Wrapper from '../assets/wrappers/Navbar';
+
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <Wrapper>
+        <h4></h4>
+    </Wrapper>
+  )
+}
+
+export default Navbar
---
+++ frontend/client/src/components/SmallSidebar.js
@@ -0,0 +1,12 @@
+import Wrapper from '../assets/wrappers/SmallSidebar';
+import React from 'react'
+
+const SmallSidebar = () => {
+  return (
+    <Wrapper>
+        <h4>small sidebar</h4>
+    </Wrapper>
+  )
+}
+
+export default SmallSidebar
