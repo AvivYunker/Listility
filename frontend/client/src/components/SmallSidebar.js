57410a1deb67cd991c34edabccdb85e7d6dceabe
AvivYunker <avivyunker@gmail.com>
2022-02-12 15:07:41 +0200

    add nav-links to small sidebar (103)

--- frontend/client/src/components/SmallSidebar.js
+++ frontend/client/src/components/SmallSidebar.js
@@ -18,7 +18,16 @@ const SmallSidebar = () => {
             <Logo/>
           </header>
           <div className="nav-links">
-            nav links
+            {links.map((link) => {
+              const {text, path, id, icon} = link
+              return
+              <NavLink to={path} key={id} onClick={toggleSidebar} className={({isActive}) => isActive ? 'nav-link active' : 'nav-link'}>
+                <span className="icon">
+                  {icon}
+                </span>
+                  {text}
+              </NavLink>
+            })}
           </div>
         </div>
       </div>
