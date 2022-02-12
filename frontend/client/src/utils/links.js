c68cbda7ed118319c6da8b7864a555e60d167ce7
AvivYunker <avivyunker@gmail.com>
2022-02-12 14:30:58 +0200

    add links in utils folder

---
+++ frontend/client/src/utils/links.js
@@ -0,0 +1,13 @@
+import { IoBarChartSharp } from 'react-icons/io5';
+import { MdQueryStats } from 'react-icons/md';
+import { FaWpforms } from 'react-cions/fa';
+import { ImProfile } from 'react-icons/im';
+
+const links = [
+    { id: 1, text: 'stats', path: '/', icon: <IoBarChartSharp/>},
+    { id: 2, text: 'all jobs', path: 'all-jobs', icon: <MdQUeryStats/>},
+    { id: 3, text: 'add job', path: 'add-job', icon: <FaWpForms/>},
+    { id: 4, text: 'profile', path: 'profile', icon: <ImProfile/>},
+]
+
+export default links
