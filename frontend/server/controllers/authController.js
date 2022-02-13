6fbf720f61c33c64db4024a6eb935a77b6a6cd13
AvivYunker <avivyunker@gmail.com>
2022-02-13 17:54:42 +0200

    complete the authentication (111)

--- frontend/server/controllers/authController.js
+++ frontend/server/controllers/authController.js
@@ -49,6 +49,7 @@ const login = async (req, res) => {
 }
 
 const updateUser = async (req, res) => {
+    console.log(req.user);
     res.send('register user');
 }
 
--- frontend/server/middleware/auth.js
+++ frontend/server/middleware/auth.js
@@ -1,10 +1,18 @@
+import jwt from 'jsonwebtoken'
 import { unAuthenticatedError } from "../errors/index.js";
 const auth = async (req, res, next) => {
     const authHeader = req.headers.authorization
-    if (!authHeader) {
+    if (!authHeader || !authHeader.startsWith('Bearer')) {
         throw new UnAuthenticatedError('Authentication Invalid');
     }
-    next()
+    const token = authHeader.split(' ')[1]
+    try {
+        const payload = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = { userId: payload.userId }
+        next()
+    } catch(error) {
+        throw new UnAuthenticatedError('AUthentication Invalid');
+    }
 }
 
 export default auth
