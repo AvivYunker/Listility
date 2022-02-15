1763fd19e5a1ca1803862ff0eaced8f374fd339c
AvivYunker <avivyunker@gmail.com>
2022-02-15 11:36:40 +0200

    edit job controller (147)

--- frontend/server/controllers/jobsController.js
+++ frontend/server/controllers/jobsController.js
@@ -15,8 +15,23 @@ const getAllJobs = async (req, res) => {
     const jobs = await Job.find({createdBy: req.user.userId});
     res.status(StatusCodes.OK).json({jobs, totalJobs: jobs.length, numOfPages: 1})
 }
-const updateJobs = async (req, res) => {
-    res.send('update job');   
+const updateJob = async (req, res) => {
+    const { position, company } = req.body;
+
+    if (!position || !company) {
+        throw new BadRequestError('Please provide all values');
+    }
+    const job = await Job.findOne({ _id: jobId })
+
+    if (!job) {
+        throw new NotFoundError(`No job with id: ${jobId}`)
+    }
+
+    const updatedJob = await Job.findOneAndUpdate({_id:jobId}, req.body, {
+        new: true,
+        runValidators: true,
+    })
+    res.status(StatusCodes.OK).json({ updatedJob })
 }
 const deleteJob = async (req, res) => {
     res.send('delete job');   
@@ -25,4 +40,4 @@ const showStats = async (req, res) => {
     res.send('show stats');   
 }
 
-export { createJob, getAllJobs, updateJobs, deleteJob, showStats }
+export { createJob, getAllJobs, updateJob, deleteJob, showStats }
