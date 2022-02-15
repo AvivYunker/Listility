f9601d73cfeba14f4199a3a9a083a6ef2c567cb7
AvivYunker <avivyunker@gmail.com>
2022-02-15 14:59:40 +0200

    edit job request

--- frontend/client/src/context/appContext.js
+++ frontend/client/src/context/appContext.js
@@ -26,6 +26,10 @@ import {
     GET_JOBS_BEGIN,
     GET_JOBS_SUCCESS,
     SET_EDIT_JOB,
+    DELETE_JOB_BEGIN,
+    EDIT_JOB_BEGIN,
+    EDIT_JOB_SUCCESS,
+    EDIT_JOB_ERROR,
 } from "./actions";
 
 const token = localStorage.getItem('token')
@@ -256,12 +260,38 @@ const AppProvider = ({children}) => {
         dispatch({ type: SET_EDIT_JOB, payload: { id }})
     }
 
-    const editJob = () => {
-        console.log('edit job')
+    const editJob = async () => {
+        dispatch({ type: DELETE_JOB_BEGIN })
+        try {
+            const {position,company,jobLocation,jobType,status} = state
+            await authFetch.patch(`/jobs/${state.editJobId}`, {
+                company,
+                position,
+                jobLocation,
+                jobType,
+                status,
+            })
+        dispatch({ type: EDIT_JOB_SUCCESS })
+        dispatch({ type: CLEAR_VALUES })
+        } catch (error) {
+            if (error.response.status === 401) return
+            dispatch ({
+                type: EDIT_JOB_ERROR,
+                payload: { msg: error.response.data.msg }
+            })
+        }
+        clearAlert()
     }
 
-    const deleteJob = (id) => {
-        console.log(`delete edit job: ${id}`);
+    const deleteJob = async (jobId) => {
+        dispatch({ type: DELETE_JOB_BEGIN })
+        try {
+            await authFetch.delete(`/jobs/${jobId}`)
+            getJobs()
+        } catch(error) {
+            console.log(error.response);
+        }
+        console.log(`delete edit job: ${jobId}`);
     }
 
     return (
--- frontend/client/src/context/reducer.js
+++ frontend/client/src/context/reducer.js
@@ -23,6 +23,10 @@ import {
     GET_JOBS_BEGIN,
     GET_JOBS_SUCCESS,
     SET_EDIT_JOB,
+    DELETE_JOB_BEGIN,
+    EDIT_JOB_BEGIN,
+    EDIT_JOB_SUCCESS,
+    EDIT_JOB_ERROR,
     } from "./actions";
 
 import { initialState } from './appContext'
@@ -226,6 +230,34 @@ const reducer = (state,action) => {
             status,
         }
     }
+    if (action.type === DELETE_JOB_BEGIN) {
+        return { ...state, isLoading: true }
+    }
+    if (action.type === DELETE_JOB_BEGIN) {
+        return {
+            ...state,
+            isLoading: true,
+        }
+    }
+    if (action.type === EDIT_JOB_SUCCESS) {
+        return {
+            ...state,
+            isLoading: false,
+            showAlert: true,
+            alertType: 'success',
+            alertText: 'Job Updated!',
+        }
+    }
+    if (action.type === EDIT_JOB_ERROR) {
+        return {
+            ...state,
+            isLoading: false,
+            showAlert: true,
+            alertType: 'danger',
+            alertText: action.payload.msg,
+        }
+    }
+
     throw new Error(`no such action : ${action.type}`);
 }
 
