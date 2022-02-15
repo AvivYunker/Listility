ad8de6c058098bf58ba1b0ff86a53334263a4969
AvivYunker <avivyunker@gmail.com>
2022-02-15 21:01:25 +0200

    configure default stats (159)

--- frontend/server/controllers/jobsController.js
+++ frontend/server/controllers/jobsController.js
@@ -3,6 +3,7 @@ import {StatusCodes} from 'http-status-codes'
 import { BadRequestError, UnAuthenticatedError } from '../errors/index.js'
 import checkPermissions from '../utils/checkPermissions.js'
 import { unAuthenticatedError } from '../errors/index.js'
+import mongoose from 'mongoose'
 
 const createJob = async (req, res) => {
     const {position, company} = req.body
@@ -53,7 +54,23 @@ const deleteJob = async (req, res) => {
     res.status(StatusCodes.OK).json({ msg: 'Success! Job removed' })
 }
 const showStats = async (req, res) => {
-    res.send('show stats');   
+    let stats = await Job.aggregate([
+        {$match:{ createdBy:mongoose.Types.ObjectId(req.user.userId) } },
+        {$group:{ _id:'$status', count: { $sum: 1} } },
+    ])
+    stats = stats.reduce((acc, curr)=>{
+        const {_id:title, count} = curr
+        acc[title] = count
+        return acc
+    },{})
+
+    const defaultStats = {
+        pending:stats.pending || 0,
+        interview:stats.interview || 0,
+        declined:stats.declined || 0
+    }
+    let monthlyApplications = []
+    res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications })
 }
 
 export { createJob, getAllJobs, updateJob, deleteJob, showStats }
