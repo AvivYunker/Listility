34ea5cdcecdf833deb6bc2a276eec94be3142883
AvivYunker <avivyunker@gmail.com>
2022-02-19 18:18:50 +0200

    re-add profile-settings link

--- application/client/src/utils/links.js
+++ application/client/src/utils/links.js
@@ -6,6 +6,7 @@ import { ImProfile } from 'react-icons/im'
 const links = [
   { id: 1, text: 'all lists', path: 'all-lists', icon: <MdQueryStats /> },
   { id: 2, text: 'add job', path: 'add-job', icon: <FaWpforms /> },
+  { id: 3, text: 'profile', path: 'profile', icon: <ImProfile /> },
 ]
 
 export default links
