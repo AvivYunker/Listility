54ea36fbf395444e6df1e4734d193cfaf1c56ad8
AvivYunker <avivyunker@gmail.com>
2022-02-20 14:40:51 +0200

    add planned lists in all-lists page

--- application/client/src/utils/links.js
+++ application/client/src/utils/links.js
@@ -5,8 +5,8 @@ import { ImProfile } from 'react-icons/im'
 
 const links = [
   { id: 1, text: 'all lists', path: 'all-lists', icon: <MdQueryStats /> },
-  { id: 2, text: 'add job', path: 'add-job', icon: <FaWpforms /> },
-  { id: 3, text: 'profile', path: 'profile', icon: <ImProfile /> },
+  { id: 2, text: 'Settings', path: 'add-job', icon: <FaWpforms /> },
+  { id: 3, text: 'About', path: 'profile', icon: <ImProfile /> },
 ]
 
 export default links
