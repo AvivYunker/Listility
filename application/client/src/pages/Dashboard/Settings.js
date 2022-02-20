7306921b0051603fc97b2c5bcf489b522c0f7e91
AvivYunker <avivyunker@gmail.com>
2022-02-20 17:44:12 +0200

    add profile-settings to settings page

--- application/client/src/pages/Dashboard/Settings.js
+++ application/client/src/pages/Dashboard/Settings.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import UserSettings from './Profile'
 
 const Settings = () => {
   return (
     <div>
-        <h1>settings page</h1>
+        <h1>application settings</h1>
+        <p>application settings should go here</p>
+        {/* <AppSettings/> */}
+        <h1>user settings</h1>
+        <UserSettings/>
     </div>
   )
 }
