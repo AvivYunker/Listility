b63f85ed4b931a1afe3ffbff61798be0313b8eb5
AvivYunker <avivyunker@gmail.com>
2022-03-12 13:41:13 +0200

    remove addjob

--- application/client/src/pages/Dashboard/index.js
+++ application/client/src/pages/Dashboard/index.js
@@ -1,5 +1,4 @@
-import AddJob from './AddJob'
 import AllLists from './AllLists'
 import Profile from './Profile'
 import SharedLayout from './SharedLayout'
-export { AllLists, Profile, SharedLayout, AddJob }
+export { AllLists, Profile, SharedLayout }
