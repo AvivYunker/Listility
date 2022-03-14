fedb95d03a7785dcbfe621e33dc932a26fb1f0e6
AvivYunker <avivyunker@gmail.com>
2022-03-14 12:22:22 +0200

    remove Application Settings

--- application/client/src/pages/Dashboard/Settings.js
+++ application/client/src/pages/Dashboard/Settings.js
@@ -1,12 +1,9 @@
 import React from 'react'
 import UserSettings from './Profile'
-import AppSettings from './AppSettings'
 
 const Settings = () => {
   return (
     <div>
-        <h1>application settings</h1>
-        <AppSettings/>
         <h1>user settings</h1>
         <UserSettings/>
     </div>
