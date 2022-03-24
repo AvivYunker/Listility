796d8261d59e9b0a5089b49a3322e8ffb518eec0
AvivYunker <avivyunker@gmail.com>
2022-03-24 14:27:33 +0200

    add edit-icon and edit-modal for list title

--- application/client/src/components/List.js
+++ application/client/src/components/List.js
@@ -6,7 +6,10 @@ import { useAppContext } from '../context/appContext'
 import Wrapper from '../assets/wrappers/Job'
 import ListInfo from './ListInfo'
 import AddTask from './AddTask'
+
 import { TiEdit } from 'react-icons/ti'
+import { MdDelete, MdEdit } from 'react-icons/md';
+
 import Addtodo from './Addtodo'
 import Tasks from './Tasks'
 import  Provider  from '../context'
@@ -19,11 +22,17 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import ShareModal from './ShareModal';
 import { updateFilterStatus } from '../slices/todoSlice';
+import styles from '../assets/styles/modules/todoItem.module.scss'
+import { MotionConfig } from 'framer-motion'
+import { motion } from 'framer-motion';
 
-
+import { deleteTodo, updateTodo } from '../slices/todoSlice';
+import toast from 'react-hot-toast';
+import UpdateListTitleModal from './UpdateListTitleModal';
 
 
 const List = ({
+  todo,
   _id,
   noteTitle,
   createdAt,
@@ -32,16 +41,43 @@ const List = ({
 
   const [modalOpen, setModalOpen] = useState(false)
   const dispatch = useDispatch()
+  const [updateModalOpen, setUpdateModalOpen] = useState(false); 
+
+
+  const handleDelete = () => {
+    dispatch(deleteTodo(todo.id));
+    toast.success('Todo Deleted Successfully');
+  };
+
+  const handleUpdate = () => {
+    setUpdateModalOpen(true);
+  };
 
   let date = moment(createdAt)
   date = date.format('Do MMM, YYYY')
   return (
     <Wrapper>
       <header>
-        <div className='info'>
-          <h5>{noteTitle} </h5>
-          <ListInfo icon={<FaCalendarAlt />} text={date} />
+      <div className={styles.todoActions}>
+          <h3>{noteTitle}</h3>
+          <div
+            className={styles.icon}
+            onClick={() => handleUpdate()}
+            onKeyDown={() => handleUpdate()}
+            tabIndex={0}
+            role="button"
+          >
+            <MdEdit />
+          </div>
         </div>
+        <UpdateListTitleModal
+          type="update"
+          modalOpen={updateModalOpen}
+          setModalOpen={setUpdateModalOpen}
+          todo={todo}
+        />
+        <br/>
+        <ListInfo icon={<FaCalendarAlt />} text={date} />
       </header>
       <main>
         <Provider>
