ed9b1754cd7e673a9318d65f173cebe2b20c72c4
AvivYunker <avivyunker@gmail.com>
2022-03-26 14:20:15 +0300

    remove QA-Automation folder

--- QA-Automation-for-Listility/example.py
+++
@@ -1,7 +0,0 @@
-import time
-from selenium import webdriver
-from selenium.webdriver.common.alert import Alert
-browser = webdriver.Chrome()
-
-browser.get('http://localhost:3000/landing')
-time.sleep(100)
